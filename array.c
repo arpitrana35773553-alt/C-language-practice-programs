cb7ff8ee482896031473ff8bc6e62f8a51f2403c
Arpit Rana <arpitrana35773553@gmail.com>
2026-03-27 21:16:54 +0530

    Added today's Program

--- array.c
+++ array.c
@@ -135,7 +135,7 @@ int main(){
     }
 }
 
-7) find out the product of digits of an prime element of the array.
+////7) find out the product of digits of an prime element of the array.
 #include <stdio.h>
 
 int prime(int n){
@@ -235,3 +235,64 @@ int main(){
     }
     printf("%s",arr[idx]);
 }
+
+///////////27-03-2026//////////////
+//CALCULATE THE SUM OF ALL NUMBERS
+#include <stdio.h>
+int main(){
+    char arr[] = {'8','7','5','2','9'};
+    int sum = 0;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i = 0;i < n;i++){
+        int digit = (int)arr[i] - 48;
+        sum += digit;
+    }
+    printf("%d",sum);
+}
+
+////CALCULATE THE SUM OF EACH DIGIT OF A NUMBER SEPARATELY
+#include <stdio.h>
+int main(){
+    char arr[][3] = {"67","78","24","53","11"};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i = 0;i < n;i++){
+        char f = arr[i][0];
+        char s = arr[i][1];
+        int first = (int)f - 48;
+        int second = (int)s -48;
+        printf("%s : %d\n",arr[i],first+second);
+    }
+}
+
+/////CALCULATE THE SUM OF EACH DIGIT OF A NUMBER SEPARATELY
+#include <stdio.h>
+int main(){
+    int arr[] = {678,56,12,34,90,102,45};
+    int n = sizeof(arr) / sizeof(int);
+    for(int i = 0;i < n;i++){
+        int sum = 0;
+        int original = arr[i];
+        while(arr[i] > 0){
+            int digit = arr[i] % 10;
+            sum += digit;
+            arr[i] = arr[i] / 10;
+        }
+        printf("%d : %d\n",original,sum);
+    } 
+}
+
+////PASSING ARRAY TO FUNCTION
+#include <stdio.h>
+void sum(int arr[],int n){
+    for(int i = 0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+}
+
+int main(){
+    int arr[] = {10,20,30,40,50,60,70,80,90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    sum(arr,n);
+}
+
+
