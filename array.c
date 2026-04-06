81acec29f4661f657d0cbab30d840149244300ff
Arpit Rana <arpitrana35773553@gmail.com>
2026-04-06 20:18:22 +0530

    Added today's program

--- array.c
+++ array.c
@@ -583,3 +583,23 @@ int main(){
 }
 
 
+/////////////06-04-2026/////////
+#include <stdio.h>
+int main(){
+    int arr[] = {10,2,6,90,87,15,67};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i = 0;i < n - 1;i++){
+        for(int j = 0;j < n - 1;j++){
+            if(arr[j] < arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for(int i = 0;i < n;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+
