30bf6c42368f36996393d5b3697280bb92872062
Arpit Rana <arpitrana35773553@gmail.com>
2026-04-09 23:02:58 +0530

    Added Today's Program

--- array.c
+++ array.c
@@ -629,3 +629,40 @@ int main(){
     }
 }
 
+
+/////COUNT THE MINIMUM NUMBER OF SWAPS REQUIRED TO SORT THE ARRAY.
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements: ");
+    for(int i = 0;i < n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int swapCount = 0;
+    for(int i = 0;i < n - 1;i++){
+        int swap = 0;
+        for(int j = 0;j < n - 1 - i;j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapCount++;
+                swap = 1;
+            }
+        }
+        if(swap == 0){
+            break;
+        }
+    }
+    printf("Sorted Array: ");
+    for(int i = 0;i < n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    printf("Number of swaps required: ");
+    printf("%d",swapCount);
+}
+
